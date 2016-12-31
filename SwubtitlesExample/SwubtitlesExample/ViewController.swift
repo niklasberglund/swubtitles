e0b6472d67ab5c2eb198001b03656b05daaca849
Niklas Berglund <niklas@asdasd.net>
2017-01-01 02:57:55 +0800

    Added example SRT file to project

--- SwubtitlesExample/SwubtitlesExample/ViewController.swift
+++ SwubtitlesExample/SwubtitlesExample/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let exampleSubtitlesUrl = Bundle.main.url(forResource: "Kung.Fu.Panda.3.2016.1080p-CHT", withExtension: "srt")
+        let subtitles = Subtitles(fileUrl: exampleSubtitlesUrl!)
     }
 
     override func didReceiveMemoryWarning() {
